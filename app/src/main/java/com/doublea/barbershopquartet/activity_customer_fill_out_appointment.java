046a1ebe0438b8c53ace7595c7056d69cddac131
trackerthehacker <39480334+trackerthehacker@users.noreply.github.com>
2018-11-09 16:36:15 -0500

    ignore this push

--- app/src/main/java/com/doublea/barbershopquartet/activity_customer_fill_out_appointment.java
+++ app/src/main/java/com/doublea/barbershopquartet/activity_customer_fill_out_appointment.java
@@ -1,5 +1,7 @@
 package com.doublea.barbershopquartet;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,4 +18,9 @@ public class activity_customer_fill_out_appointment extends AppCompatActivity {
 
     }
 
+
+    public void onClickReserveAppointment(View view) {
+
+        Intent goToMain = new Intent(this, ActivityMain.class);
+    }
 }
