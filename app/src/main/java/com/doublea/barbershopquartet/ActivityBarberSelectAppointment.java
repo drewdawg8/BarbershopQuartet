9fa31b0b36db600a784dae23c1dfe1b6b000c936
ahmedhhw <ahmedhhw@gmail.com>
2018-11-09 14:46:34 -0500

    Created templatees for all GUI's

---
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberSelectAppointment.java
@@ -0,0 +1,13 @@
+package com.doublea.barbershopquartet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBarberSelectAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barber_select_appointment);
+    }
+}
---
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityCustomerAppointmentRequest.java
@@ -0,0 +1,13 @@
+package com.doublea.barbershopquartet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCustomerAppointmentRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_appointment_request);
+    }
+}
