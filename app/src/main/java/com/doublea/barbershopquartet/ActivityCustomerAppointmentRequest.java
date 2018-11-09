83fe02e19f71363cf3b455a87560f87c3e60b30a
trackerthehacker <39480334+trackerthehacker@users.noreply.github.com>
2018-11-09 16:19:18 -0500

    Edited ActivityCustomerAppointmentRequest with ability to go to next interface, and edited the interface for activity_customer_appointment_request

--- app/src/main/java/com/doublea/barbershopquartet/ActivityCustomerAppointmentRequest.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityCustomerAppointmentRequest.java
@@ -1,7 +1,9 @@
 package com.doublea.barbershopquartet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityCustomerAppointmentRequest extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ActivityCustomerAppointmentRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_appointment_request);
+    }
+
+    public void onClickRegister(View view) {
+        Intent startNewActivity = new Intent(this, activity_customer_fill_out_appointment.class);
     }
 }
