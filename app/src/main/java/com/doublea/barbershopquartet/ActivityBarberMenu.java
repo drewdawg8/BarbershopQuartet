937398892a7eea7276d20d42816eb15f2c15ca84
Drew Stewart <dstewa17@masonlive.gmu.edu>
2018-11-13 13:48:41 -0500

    testing..

--- app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-//
+
 public class ActivityBarberMenu extends AppCompatActivity {
 
     @Override
