c8a0465318f7bad2acf57f5b2052ad19c1b8c060
Drew Stewart <dstewa17@masonlive.gmu.edu>
2018-11-13 13:37:45 -0500

    added ids for BarberMenu buttons and created skeleton methods for them in the java src file for BarberMenu

--- app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
@@ -2,6 +2,8 @@ package com.doublea.barbershopquartet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityBarberMenu extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ActivityBarberMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber_menu);
+    }
+    final Button logoutBtn = findViewById(R.id.logout);
+    final Button manageBtn = findViewById(R.id.manageSchedule);
+    final Button appBtn = findViewById(R.id.selectApp);
+
+    public  void onClickLogout(View v){
+
+    }
+
+    public void onClickManageSchedule(View v){
+
+    }
+
+    public void onClickSelectAppointments(View v){
+
     }
 }
