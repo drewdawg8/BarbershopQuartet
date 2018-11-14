0eed98fcdd657daaeac5266ec182b8e715235e6d
ahmedhhw <ahmedhhw@gmail.com>
2018-11-14 17:08:04 -0500

    Pre testing fireBase interaction written

--- app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/FirebaseInteraction.java
+++ app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/FirebaseInteraction.java
@@ -1,4 +1,64 @@
 package com.doublea.barbershopquartet.BackgroundTools;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class FirebaseInteraction {
+    private FirebaseDatabase database;
+    public FirebaseInteraction(){
+        database = FirebaseDatabase.getInstance();
+    }
+
+
+    /**
+     * Reads from firebase database
+     * @param path Path to read from
+     * @param listen Listener that allows data to be communicated back
+     */
+    public void read(String path, final FirebaseReadListener listen){
+        DatabaseReference currentPath = database.getReference(path);
+        currentPath.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listen.onSuccess(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listen.onFailure();
+            }
+        });
+    }
+
+    /**
+     * Reads when called and when change occurs at particular path
+     * @param path
+     * @param listen
+     */
+    public void readOnUpdate(String path, final FirebaseReadListener listen){
+        DatabaseReference currentPath = database.getReference(path);
+        currentPath.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listen.onSuccess(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listen.onFailure();;
+            }
+        });
+    }
+    /**
+     * Writes strings to firebase
+     * @param path Path of string
+     * @param value String to be written
+     */
+    public void write(String path, String value){
+        DatabaseReference currentPath = database.getReference(path);
+        currentPath.setValue(value);
+    }
 }
--- app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/FirebaseReadListener.java
+++ app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/FirebaseReadListener.java
@@ -1,4 +1,8 @@
 package com.doublea.barbershopquartet.BackgroundTools;
 
+import com.google.firebase.database.DataSnapshot;
+
 public interface FirebaseReadListener {
+    public void onSuccess(DataSnapshot data);
+    public void onFailure();
 }
