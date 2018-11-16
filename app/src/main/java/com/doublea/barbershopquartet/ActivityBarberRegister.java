ccdcb9c0e4d2d875a856097b32ac371d2da30296
ahmedhhw <ahmedhhw@gmail.com>
2018-11-16 15:38:38 -0500

    Now you may register :D :D :D

--- app/src/main/java/com/doublea/barbershopquartet/ActivityBarberRegister.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberRegister.java
@@ -1,7 +1,10 @@
 package com.doublea.barbershopquartet;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -9,6 +12,11 @@ import android.widget.Toast;
 import com.doublea.barbershopquartet.BackgroundTools.Barber;
 import com.doublea.barbershopquartet.BackgroundTools.FirebaseInteraction;
 import com.doublea.barbershopquartet.BackgroundTools.FirebaseReadListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 
 public class ActivityBarberRegister extends AppCompatActivity {
@@ -21,6 +29,7 @@ public class ActivityBarberRegister extends AppCompatActivity {
     private EditText confirmPassWord;
     private EditText description;
     private FirebaseInteraction firebase;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +61,7 @@ public class ActivityBarberRegister extends AppCompatActivity {
         confirmPassWord = findViewById(R.id.register_edit_text_confirm_password);
         description = findViewById(R.id.register_edit_text_description);
         firebase = new FirebaseInteraction();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     /**
@@ -63,9 +73,34 @@ public class ActivityBarberRegister extends AppCompatActivity {
     public void onClickRegister(View view) {
         if (!validInputs())
             return;
+        registerBarber(email.getText().toString(),password.getText().toString());
+    }
+
+    private void registerBarber(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user.getUid());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(String uid) {
         Barber barber = new Barber(firstName.getText().toString(), lastName.getText().toString(),
                 phoneNumber.getText().toString(), email.getText().toString(),
                 description.getText().toString());
+        firebase.write("Barbers/" + uid, barber);
+        startActivity(new Intent(this, ActivityBarberMenu.class));
+
     }
 
     private boolean validInputs() {
@@ -102,5 +137,6 @@ public class ActivityBarberRegister extends AppCompatActivity {
     }
 
     public void onClickswitchToLogIn(View view) {
+        startActivity(new Intent(this, ActivityBarberLogin.class));
     }
 }
