21ced18f34063c7219423a764372f39d33466d98
trackerthehacker <39480334+trackerthehacker@users.noreply.github.com>
2018-11-24 17:49:16 -0500

    Added toString to Customer.java, and completed xml for activity_barber_manage_appointment and the backend for ActivityBarberManageAppointment

--- app/src/main/java/com/doublea/barbershopquartet/ActivityBarberManageAppointment.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberManageAppointment.java
@@ -1,13 +1,53 @@
 package com.doublea.barbershopquartet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.doublea.barbershopquartet.BackgroundTools.FirebaseInteraction;
+import com.doublea.barbershopquartet.BackgroundTools.TimeSlot;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.doublea.barbershopquartet.ActivityCustomerAppointmentRequest.timeSlot;
 
 public class ActivityBarberManageAppointment extends AppCompatActivity {
 
+    private TimeSlot localTimeSlot = ActivityBarberSelectAppointment.selectedTimeSlot;
+    private FirebaseInteraction firebaseInteraction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber_manage_appointment);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeInformation();
+        firebaseInteraction = new FirebaseInteraction();
+    }
+
+    public void onClickRemoveAppointment(View view) {
+        TimeSlot slot = new TimeSlot(localTimeSlot.getMonth(), localTimeSlot.getDay(), localTimeSlot.getHour(),
+                localTimeSlot.getMinute(), null);
+        firebaseInteraction.writeTimeslot(slot, FirebaseAuth.getInstance().getUid());
+        startActivity(new Intent(this,ActivityBarberSelectAppointment.class));
+    }
+
+    public void initializeInformation(){
+        TextView textView = (TextView)findViewById(R.id.edit_text_name);
+        textView.setText(ActivityBarberSelectAppointment.selectedTimeSlot.getAppointment().getCustomer().toString());
+        textView = (TextView)findViewById(R.id.edit_text_email);
+        textView.setText(ActivityBarberSelectAppointment.selectedTimeSlot.getAppointment().getCustomer().getEmail());
+        textView = (TextView)findViewById(R.id.edit_text_phone_number);
+        textView.setText(ActivityBarberSelectAppointment.selectedTimeSlot.getAppointment().getCustomer().getPhoneNumber());
+        textView = (TextView)findViewById(R.id.edit_text_time_slot);
+        textView.setText(ActivityBarberSelectAppointment.selectedTimeSlot.toString());
+        textView = (TextView)findViewById(R.id.edit_text_notes);
+        textView.setText(ActivityBarberSelectAppointment.selectedTimeSlot.getAppointment().getNotes());
     }
 }
--- app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/Customer.java
+++ app/src/main/java/com/doublea/barbershopquartet/BackgroundTools/Customer.java
@@ -4,4 +4,8 @@ public class Customer extends User {
     public Customer(String firstName, String lastName, String phoneNumber, String email){
         super(firstName, lastName, phoneNumber, email);
     }
+
+    public String toString(){
+        return getFirstName() + " " + getLastName();
+    }
 }
