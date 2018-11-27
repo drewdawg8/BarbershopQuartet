b24ebb3cac0e5d9c0754c3d4d819a05c87c6dfa4
trackerthehacker <39480334+trackerthehacker@users.noreply.github.com>
2018-11-27 00:05:48 -0500

    Added javadoc to ActivityBarberMenu

--- app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
+++ app/src/main/java/com/doublea/barbershopquartet/ActivityBarberMenu.java
@@ -14,6 +14,11 @@ public class ActivityBarberMenu extends AppCompatActivity {
     private Button manageBtn;
     private Button appBtn;
     private FirebaseAuth mAuth;
+
+    /**
+     * Method triggered on creation of Activity.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,10 @@ public class ActivityBarberMenu extends AppCompatActivity {
         initializeVarbles();
     }
 
+    /**
+     * Method to help initialize variables in the class to buttons and the
+     * FirebaseAuth variable.
+     */
     private void initializeVarbles() {
         logoutBtn = findViewById(R.id.logout);
         manageBtn = findViewById(R.id.manageSchedule);
@@ -29,16 +38,27 @@ public class ActivityBarberMenu extends AppCompatActivity {
     }
 
 
-
+    /**
+     * Method to handle user Logging out.
+     * @param v
+     */
     public  void onClickLogout(View v){
         mAuth.signOut();
         startActivity(new Intent(this,ActivityBarberLogin.class));
     }
 
+    /**
+     * Method to handle barber clicking manage schedule button.
+     * @param v
+     */
     public void onClickManageSchedule(View v){
         startActivity(new Intent(ActivityBarberMenu.this, ActivityManageSchedule.class));
     }
 
+    /**
+     * Method to handle barber selection of an Appointment.
+     * @param v
+     */
     public void onClickSelectAppointments(View v){
         startActivity(new Intent(ActivityBarberMenu.this, ActivityBarberSelectAppointment.class));
 
