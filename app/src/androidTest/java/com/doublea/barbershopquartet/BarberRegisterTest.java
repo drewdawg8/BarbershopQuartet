af942dd8267d8d5aa1cd4802d8c00202275a66bf
Zach <zachary.parn@gmail.com>
2018-12-02 16:50:28 -0500

    Updated BarberRegisterTest. Needs testing.

--- app/src/androidTest/java/com/doublea/barbershopquartet/BarberRegisterTest.java
+++ app/src/androidTest/java/com/doublea/barbershopquartet/BarberRegisterTest.java
@@ -2,8 +2,13 @@ package com.doublea.barbershopquartet;
 
 import android.support.test.rule.ActivityTestRule;
 
+import com.doublea.barbershopquartet.BackgroundTools.FirebaseInteraction;
+import com.doublea.barbershopquartet.BackgroundTools.FirebaseReadListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -12,32 +17,26 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class BarberRegisterTest {
 
-    public void sleep(){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public void sleep(int x){
-        try {
-            Thread.sleep(1000 * x);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    private FirebaseAuth auth;
+    private FirebaseInteraction firebase;
 
     @Rule
     public ActivityTestRule mActivityRule = new ActivityTestRule<>(ActivityMain.class);
 
+    @Before
+    public void initializeFirebaseObjects() {
+        auth = FirebaseAuth.getInstance();
+        firebase = new FirebaseInteraction();
+    }
+
     @Test
     public void testBarberRegister() {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-
         if (auth != null) {
             auth.signOut();
         }
@@ -58,5 +57,76 @@ public class BarberRegisterTest {
         onView(withId(R.id.register_button_register)).perform(click());
         sleep(10);
         assertTrue(auth != null);
+        onView(withId(R.id.logout)).perform(click());
+    }
+
+    @Test
+    public void testNewBarberLogin() {
+        if (auth != null) {
+            auth.signOut();
+        }
+
+        onView(withId(R.id.main_button_barber)).perform(click());
+        sleep();
+        onView(withId(R.id.login_edit_text_email)).perform(replaceText("firstname@email.com"));
+        onView(withId(R.id.login_edit_text_password)).perform(replaceText("test123"));
+        onView(withId(R.id.btn_login)).perform(click());
+        sleep();
+        assertTrue(auth != null);
+        assertTrue(auth.getCurrentUser().getEmail().equals("firstname@email.com"));
+        //onView(withId(R.id.logout)).perform(click());
+    }
+
+    @Test
+    public void testDatabaseInitialization() {
+        String uid = auth.getUid();
+
+        String path = "Barbers/"+ uid;
+        firebase.read(path, new FirebaseReadListener() {
+            @Override
+            public void onSuccess(DataSnapshot data) {
+                assertEquals("Firstname", data.child("firstName").getValue().toString());
+                assertEquals("Lastname", data.child("lastName").getValue().toString());
+                assertEquals("7035551234", data.child("phoneNumber").getValue().toString());
+                assertEquals("firstname@email.com", data.child("email").getValue().toString());
+                assertEquals("This is a description", data.child("description").getValue().toString());
+
+                for (int i = 1; i <= 12; i++) {
+                    assertTrue(data.child(Integer.toString(i)).exists());
+                }
+            }
+
+            @Override
+            public void onFailure() {
+                fail("Could not read from database");
+            }
+        });
+    }
+
+    @After
+    public void deleteTestBarber() {
+        String uid = auth.getUid();
+        String path = "Barbers/" + uid;
+        firebase.write(path, null);
+        auth.getCurrentUser().delete();
+        sleep(3);
+    }
+
+    // sleep for 1 second
+    private void sleep(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // sleep for x seconds
+    private void sleep(int x){
+        try {
+            Thread.sleep(1000 * x);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
